6420a760f86b992d4eca2f8b33072ba75bb88935
Paul Nikitin <top@neox.ru>
2026-03-14 12:59:34 +0300

    feat(part): создана сущность страниц/разделов

---
+++ src/Entity/Part.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Ideal\Entity;
+
+use Ideal\Repository\PartRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PartRepository::class)]
+class Part
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $prevStructure = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrevStructure(): ?string
+    {
+        return $this->prevStructure;
+    }
+
+    public function setPrevStructure(string $prevStructure): static
+    {
+        $this->prevStructure = $prevStructure;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
